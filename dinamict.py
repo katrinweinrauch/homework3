0ceb0c5e4f6ccf1a058add054cdf980c885f1ce6
katrinweinrauch <katrinweinrauch1@yandex.ru>
2024-07-29 23:42:19 +0500

    Add files via upload

---
+++ dinamict.py
@@ -0,0 +1,8 @@
+name = 'Ekaterina'
+print(name, type(name))
+age = 28
+print(age, type(age))
+new_age = 1
+print(age + new_age, type(age + new_age))
+is_student = True
+print(is_student, type(is_student))
